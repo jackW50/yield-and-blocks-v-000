1c47978759338a2c772a0cd45a9e778c70d6adff
John Werenski <jtwerenski@gmail.com>
2018-09-12 20:16:01 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -12,8 +12,8 @@ end
 
 # call your method here!
 
-hello_t() do |name|
-  if name.start_with?("T")
-    puts "Hi, #{name}"
-  end 
-end 
+#hello_t() do |name|
+ # if name.start_with?("T")
+ #   puts "Hi, #{name}"
+ # end 
+#end 
